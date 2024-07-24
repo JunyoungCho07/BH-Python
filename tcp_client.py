f7eb7932797270ef40eaf96d0d76e299a734c022
JunyoungCho07 <chojunyoung070523@gmail.com>
2024-07-24 02:36:26 -0400

    modified:   tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -1,13 +1,19 @@
 import socket
+
 HOST = "www.google.com"
 PORT = 80
+
 #made socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 #client connect
 client.connect((HOST,PORT))
+
 #send some data
-client.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
+client.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')#\r\n -> like enter key
+
 #recieve data
 responce = client.recv(4096)
+
 print(responce.decode('utf-8'))
 client.close()
